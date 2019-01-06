cddd13a13fa82d4df3476068ba505ca9bb50d73f
James Woolfenden <James.Woolfenden@gmail.com>
2019-01-06 08:47:41 +0000

    version 2

--- examples/exampleA/module.security_buckets.tf
+++ examples/exampleA/module.security_buckets.tf
@@ -1,5 +1,6 @@
 module "security_buckets" {
-  source                  = "../../"
+  source                  = "JamesWoolfenden/security-buckets/aws"
+  version                 = "0.0.2"
   s3_bucket_force_destroy = "${var.s3_bucket_force_destroy}"
   key_names               = "${var.key_names}"
   common_tags             = "${var.common_tags}"
