c759a6c0a2f0419c2329ed7b6c04d211e359620f
James Woolfenden <James.Woolfenden@gmail.com>
2019-01-20 12:44:20 +0000

    migrate to build-harness

--- variables.tf
+++ variables.tf
@@ -9,3 +9,10 @@ variable "key_names" {
 variable "common_tags" {
   type = "map"
 }
+
+# this is enables you to add dependancies
+variable depends_on {
+  default     = []
+  description = "This is a way to make a module depends on, which isnt built in."
+  type        = "list"
+}
