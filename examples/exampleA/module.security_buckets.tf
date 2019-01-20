7b36fc3b666eebcbe4ead8f9ced8616124b20b73
James Woolfenden <James.Woolfenden@gmail.com>
2019-01-20 12:46:32 +0000

    migrate to build-harness

--- examples/exampleA/module.security_buckets.tf
+++ examples/exampleA/module.security_buckets.tf
@@ -1,6 +1,6 @@
 module "security_buckets" {
   source                  = "JamesWoolfenden/security-buckets/aws"
-  version                 = "0.0.2"
+  version                 = "0.0.4"
   s3_bucket_force_destroy = "${var.s3_bucket_force_destroy}"
   key_names               = "${var.key_names}"
   common_tags             = "${var.common_tags}"
