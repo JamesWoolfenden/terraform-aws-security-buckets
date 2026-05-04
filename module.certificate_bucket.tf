d377e8a7c3b03a43427ac6ba9203d098a2f19786
James Woolfenden <james.woolfenden@gmail.com>
2026-05-04 11:48:38 +0100

    chore: pin dependencies to immutable SHAs via ghat

--- module.certificate_bucket.tf
+++ module.certificate_bucket.tf
@@ -1,6 +1,5 @@
 module "certificates_bucket" {
-  source                  = "JamesWoolfenden/s3/aws"
-  version                 = "0.4.34"
+  source = "git::https://github.com/JamesWoolfenden/terraform-aws-s3.git?ref=d407883afde3d84e81da4e703da4787edd1f58cd" #v0.4.34
   s3_bucket_name          = "certificates-${data.aws_caller_identity.current.account_id}"
   s3_bucket_policy        = data.template_file.policy.rendered
   s3_bucket_force_destroy = var.s3_bucket_force_destroy
